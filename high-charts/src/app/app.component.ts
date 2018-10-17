fc78b4ad3a5a070cfd22504a7c44e4b3c9035730
Ronnie Mathew <ronnie@bricksimple.com>
2018-10-17 16:38:04 -0400

    Moved the workhorse code to a separate function updateChartData

--- high-charts/src/app/app.component.ts
+++ high-charts/src/app/app.component.ts
@@ -9,7 +9,7 @@ import { CsvUtilsService } from './modules/shared/services/csv-utils.service';
     styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
-    title = 'app';
+    data = null;
     highCharts = Highcharts;
     chartOptions = null;
 
@@ -19,67 +19,60 @@ export class AppComponent implements OnInit {
     ) {}
 
     ngOnInit() {
-        // const csv = 
-        //     'BINS, TICKS_BELOW, TICKS_MEETS, TICKS_ABOVE\n' +
-        //     '1, 64, 60, 67\n' +
-        //     '2, 70, 66, 70\n' +
-        //     '3, 76, 71, 77\n' +
-        //     '4, 82, 83, 84\n' +
-        //     '5, 82, 83, 84\n' +
-        //     '6, 82, 83, 84\n' +
-        //     '7, 87, 87, 87';
-
         this.http.get('assets/kf-payh-poc-charts-sample-data.csv', { responseType: 'text' }).subscribe((csvData) => {
             const data = this.csvUtils.csvToJSON(csvData);
-            // console.log('data', data);
-
-            let minYears = Number.MAX_VALUE;
-            let maxYears = Number.MIN_VALUE;
-            data.forEach((record) => {
-                const yearsEmployed = +record['YearsEmployed'];
-                if (yearsEmployed < minYears) {
-                    minYears = yearsEmployed;
-                }
-                
-                if (yearsEmployed > maxYears) {
-                    maxYears = yearsEmployed;
-                }
-            });
-
-            const seriesTicksBelow = new Array(maxYears-1).fill(0, 0, maxYears-1);
-            const seriesTicksMeets = new Array(maxYears-1).fill(0, 0, maxYears-1);
-            const seriesTicksAbove = new Array(maxYears-1).fill(0, 0, maxYears-1);
-
-            data.forEach((record) => {
-                const yearsEmployed = +record['YearsEmployed'];
-                const index = yearsEmployed - 1;
-                const expectations = +record['Expectations'];
-                switch (expectations) {
-                case 1: seriesTicksBelow[index] = seriesTicksBelow[index] ? seriesTicksBelow[index] + 1 : 1; break;
-                case 2: seriesTicksMeets[index] = seriesTicksMeets[index] ? seriesTicksMeets[index] + 1 : 1; break;
-                case 3: seriesTicksAbove[index] = seriesTicksAbove[index] ? seriesTicksAbove[index] + 1 : 1; break;
-                }
-            });
-
-            console.log('seriesTicksBelow', seriesTicksBelow);
-            console.log('seriesTicksMeets', seriesTicksMeets);
-            console.log('seriesTicksAbove', seriesTicksAbove);
-
-            const categories = [];
-            for (let i = minYears; i <= maxYears; i++) {
-                categories.push(i);
-            }
-
-            console.log('categories', categories);
-
-            const series = [
-                { name: 'Below Expectation', data: seriesTicksBelow },
-                { name: 'Meets Expectation', data: seriesTicksMeets },
-                { name: 'Above Expectation', data: seriesTicksAbove }
-            ];
-
-            this.chartOptions = this.getChartOptions(categories, series);
+            this.data = data;
+            this.updateChartData();
         });
+    }
+
+    updateChartData() {
+        let minYears = Number.MAX_VALUE;
+        let maxYears = Number.MIN_VALUE;
+        this.data.forEach((record) => {
+            const yearsEmployed = +record['YearsEmployed'];
+            if (yearsEmployed < minYears) {
+                minYears = yearsEmployed;
+            }
+            
+            if (yearsEmployed > maxYears) {
+                maxYears = yearsEmployed;
+            }
+        });
+
+        const seriesTicksBelow = new Array(maxYears-1).fill(0, 0, maxYears-1);
+        const seriesTicksMeets = new Array(maxYears-1).fill(0, 0, maxYears-1);
+        const seriesTicksAbove = new Array(maxYears-1).fill(0, 0, maxYears-1);
+
+        this.data.forEach((record) => {
+            const yearsEmployed = +record['YearsEmployed'];
+            const index = yearsEmployed - 1;
+            const expectations = +record['Expectations'];
+            switch (expectations) {
+            case 1: seriesTicksBelow[index] = seriesTicksBelow[index] ? seriesTicksBelow[index] + 1 : 1; break;
+            case 2: seriesTicksMeets[index] = seriesTicksMeets[index] ? seriesTicksMeets[index] + 1 : 1; break;
+            case 3: seriesTicksAbove[index] = seriesTicksAbove[index] ? seriesTicksAbove[index] + 1 : 1; break;
+            }
+        });
+
+        // console.log('seriesTicksBelow', seriesTicksBelow);
+        // console.log('seriesTicksMeets', seriesTicksMeets);
+        // console.log('seriesTicksAbove', seriesTicksAbove);
+
+        const categories = [];
+        for (let i = minYears; i <= maxYears; i++) {
+            categories.push(i);
+        }
+
+        // console.log('categories', categories);
+
+        const series = [
+            { name: 'Below Expectation', data: seriesTicksBelow },
+            { name: 'Meets Expectation', data: seriesTicksMeets },
+            { name: 'Above Expectation', data: seriesTicksAbove }
+        ];
+
+        this.chartOptions = this.getChartOptions(categories, series);
     }
 
     getChartOptions(categories, series) {
@@ -90,9 +83,6 @@ export class AppComponent implements OnInit {
             title: {
                 text: 'Pay for Performance Over Time'
             },
-            // subtitle: {
-            //     text: 'Source: <a href="https://en.wikipedia.org/wiki/World_population">Wikipedia.org</a>'
-            // },
             xAxis: {
                 categories: categories,
                 title: {
