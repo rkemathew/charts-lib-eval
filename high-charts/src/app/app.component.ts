827bba818ded21bde139043c69cab0d4d63e8b9e
Ronnie Mathew <ronnie@bricksimple.com>
2018-10-17 16:31:13 -0400

    Made modifications to have the chart be created from the CSV data. Yet to add Filters

--- high-charts/src/app/app.component.ts
+++ high-charts/src/app/app.component.ts
@@ -11,70 +11,7 @@ import { CsvUtilsService } from './modules/shared/services/csv-utils.service';
 export class AppComponent implements OnInit {
     title = 'app';
     highCharts = Highcharts;
-    series = [{
-        name: 'Below Expectation',
-        data: [64, 70, 76, 82, 82, 82, 87]
-    }, {
-        name: 'Meets Expectation',
-        data: [60, 66, 71, 83, 83, 83, 87]
-    }, {
-        name: 'Above Expectation',
-        data: [67, 70, 77, 84, 84, 84, 87]
-    }];
-
-    chartOptions = {
-        chart: {
-            type: 'column'
-        },
-        title: {
-            text: 'Pay for Performance Over Time'
-        },
-        // subtitle: {
-        //     text: 'Source: <a href="https://en.wikipedia.org/wiki/World_population">Wikipedia.org</a>'
-        // },
-        xAxis: {
-            categories: [1, 2, 3, 4, 5, 6, 7],
-            title: {
-                text: 'Years Employed'
-            }
-        },
-        yAxis: {
-            min: 0,
-            title: {
-                text: 'Compensation (millions)',
-                align: 'high'
-            },
-            labels: {
-                overflow: 'justify',
-                formatter: (ref) => ref.value + 'M'
-            }
-        },
-        tooltip: {
-            valueSuffix: ' millions'
-        },
-        plotOptions: {
-            bar: {
-                dataLabels: {
-                    enabled: true
-                }
-            }
-        },
-        legend: {
-            layout: 'vertical',
-            align: 'right',
-            verticalAlign: 'top',
-            x: -40,
-            y: 80,
-            floating: true,
-            borderWidth: 1,
-            backgroundColor: ((Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF'),
-            shadow: true
-        },
-        credits: {
-            enabled: false
-        },
-        series: this.series
-    };
+    chartOptions = null;
 
     constructor(
         private csvUtils: CsvUtilsService,
@@ -92,16 +29,112 @@ export class AppComponent implements OnInit {
         //     '6, 82, 83, 84\n' +
         //     '7, 87, 87, 87';
 
-        this.http.get('assets/kf-payh-poc-charts-sample-data.csv', { responseType: 'text' }).subscribe((data) => {
-            const csv = data;
-            const csvAsJSON = this.csvUtils.csvToJSON(csv);
-            console.log('csvAsJSON', csvAsJSON);
-    
-            setTimeout(() => {
-                this.highCharts.charts[0].series[0].setData([44, 50, 32, 60, 64, 15, 34]);
-                this.highCharts.charts[0].series[1].setData([37, 26, 79, 44, 39, 92, 67]);
-                this.highCharts.charts[0].series[2].setData([70, 27, 33, 90, 65, 41, 73]);
-            }, 5000);
+        this.http.get('assets/kf-payh-poc-charts-sample-data.csv', { responseType: 'text' }).subscribe((csvData) => {
+            const data = this.csvUtils.csvToJSON(csvData);
+            // console.log('data', data);
+
+            let minYears = Number.MAX_VALUE;
+            let maxYears = Number.MIN_VALUE;
+            data.forEach((record) => {
+                const yearsEmployed = +record['YearsEmployed'];
+                if (yearsEmployed < minYears) {
+                    minYears = yearsEmployed;
+                }
+                
+                if (yearsEmployed > maxYears) {
+                    maxYears = yearsEmployed;
+                }
+            });
+
+            const seriesTicksBelow = new Array(maxYears-1).fill(0, 0, maxYears-1);
+            const seriesTicksMeets = new Array(maxYears-1).fill(0, 0, maxYears-1);
+            const seriesTicksAbove = new Array(maxYears-1).fill(0, 0, maxYears-1);
+
+            data.forEach((record) => {
+                const yearsEmployed = +record['YearsEmployed'];
+                const index = yearsEmployed - 1;
+                const expectations = +record['Expectations'];
+                switch (expectations) {
+                case 1: seriesTicksBelow[index] = seriesTicksBelow[index] ? seriesTicksBelow[index] + 1 : 1; break;
+                case 2: seriesTicksMeets[index] = seriesTicksMeets[index] ? seriesTicksMeets[index] + 1 : 1; break;
+                case 3: seriesTicksAbove[index] = seriesTicksAbove[index] ? seriesTicksAbove[index] + 1 : 1; break;
+                }
+            });
+
+            console.log('seriesTicksBelow', seriesTicksBelow);
+            console.log('seriesTicksMeets', seriesTicksMeets);
+            console.log('seriesTicksAbove', seriesTicksAbove);
+
+            const categories = [];
+            for (let i = minYears; i <= maxYears; i++) {
+                categories.push(i);
+            }
+
+            console.log('categories', categories);
+
+            const series = [
+                { name: 'Below Expectation', data: seriesTicksBelow },
+                { name: 'Meets Expectation', data: seriesTicksMeets },
+                { name: 'Above Expectation', data: seriesTicksAbove }
+            ];
+
+            this.chartOptions = this.getChartOptions(categories, series);
         });
+    }
+
+    getChartOptions(categories, series) {
+        return {
+            chart: {
+                type: 'column'
+            },
+            title: {
+                text: 'Pay for Performance Over Time'
+            },
+            // subtitle: {
+            //     text: 'Source: <a href="https://en.wikipedia.org/wiki/World_population">Wikipedia.org</a>'
+            // },
+            xAxis: {
+                categories: categories,
+                title: {
+                    text: 'Years Employed'
+                }
+            },
+            yAxis: {
+                min: 0,
+                title: {
+                    text: 'Compensation (millions)',
+                    align: 'high'
+                },
+                labels: {
+                    overflow: 'justify',
+                    formatter: (ref) => ref.value + 'M'
+                }
+            },
+            tooltip: {
+                valueSuffix: ' millions'
+            },
+            plotOptions: {
+                bar: {
+                    dataLabels: {
+                        enabled: true
+                    }
+                }
+            },
+            legend: {
+                layout: 'vertical',
+                align: 'right',
+                verticalAlign: 'top',
+                x: -40,
+                y: 80,
+                floating: false,
+                borderWidth: 1,
+                backgroundColor: ((Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF'),
+                shadow: true
+            },
+            credits: {
+                enabled: false
+            },
+            series: series
+        };
     }
 }
